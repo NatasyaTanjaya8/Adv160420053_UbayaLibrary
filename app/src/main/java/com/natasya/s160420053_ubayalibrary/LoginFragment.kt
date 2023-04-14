eca8c12440a4a586962497ff000d43c793974af5
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-04-14 15:47:11 +0700

    14 April 2023 15:46 WIB

--- app/src/main/java/com/natasya/s160420053_ubayalibrary/LoginFragment.kt
+++ app/src/main/java/com/natasya/s160420053_ubayalibrary/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,17 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonCreateAccount.setOnClickListener {
+            var action = LoginFragmentDirections.actionRegisterFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+        buttonSignIn.setOnClickListener {
+            var action = LoginFragmentDirections.actionHomeFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
