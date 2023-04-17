889c0a851c07fde5bec9574dc7f023892e0a0748
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-04-17 15:44:51 +0700

    17 April 2023 15:44 WIB

--- app/src/main/java/com/natasya/s160420053_ubayalibrary/MainActivity.kt
+++ app/src/main/java/com/natasya/s160420053_ubayalibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.natasya.s160420053_ubayalibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -10,11 +11,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        drawerLayout = findViewById(R.id.drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController)
         bottomNav.setupWithNavController(navController)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //return super.onSupportNavigateUp()
+        //return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
