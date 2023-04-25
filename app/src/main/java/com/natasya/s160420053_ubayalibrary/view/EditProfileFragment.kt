bb49bb38189c9aca55dbc4eec96738643d4db4cd
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-04-25 17:01:04 +0700

    25 April 2023 17:00 WIB

--- app/src/main/java/com/natasya/s160420053_ubayalibrary/view/EditProfileFragment.kt
+++ app/src/main/java/com/natasya/s160420053_ubayalibrary/view/EditProfileFragment.kt
@@ -7,21 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.natasya.s160420053_ubayalibrary.R
-import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
-class ProfileFragment : Fragment() {
+class EditProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        buttonEdit.setOnClickListener {
-            var action = ProfileFragmentDirections.actionEditProfileFragment()
+        buttonDone.setOnClickListener {
+            var action = EditProfileFragmentDirections.actionItemProfile()
             Navigation.findNavController(it).navigate(action)
         }
     }
